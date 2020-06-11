75ac73f4b4b8053df6c855a0e1aa654f2f560e27
Hafizh Raihan <62530036+hxfxzhrd@users.noreply.github.com>
2020-06-12 00:14:45 +0700

    Add files via upload

---
+++ ContohProgram/operasiaritmatika.ml
@@ -0,0 +1,31 @@
+# Aritmetika
+
+# Pengurangan
+9-6
+
+# Penjumlahan
+4+9
+
+# Perkalian
+2*2
+
+# Pembagian
+6/3
+
+kuy "Ada cara yang lain, caranya adalah sebagai berikut : "
+
+#dengan mendeklarasikan nilai variabel lalu bisa dioperasikan
+x=8
+y=4
+
+# Pengurangan
+kuy x-y
+
+# Penjumlahan
+kuy x+y
+
+# Perkalian 
+kuy x*y
+
+# Pembagian
+kuy x/y
---
+++ ContohProgram/opifelse.ml
@@ -0,0 +1,6 @@
+kuy "contoh operasi if else"
+
+a = "benar"
+b = "salah"
+
+jika x == 2	maka kuy a lainnya kuy b
